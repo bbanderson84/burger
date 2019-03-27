fdafa56314906f8ba5be2e1563d6b7a0b3a7f7b8
bbanderson84 <bbanderson84@gmail.com>
2019-03-27 11:48:37 -0600

    created schema and seed file, updated connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Da Whoppa');
+INSERT INTO burgers (burger_name) VALUES ('Da Big KAHOONA');
+INSERT INTO burgers (burger_name) VALUES ('Da Double Decka')
+INSERT INTO burgers (burger_name) VALUES ('Da Veggie Smedgie')
